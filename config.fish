0bb2ffce05b96a1b2e43bf047a661dffccf1fa5e
Alex Rodionov <p0deje@gmail.com>
2014-01-19 12:30:25 +0700

    [fish] much doge git

--- config.fish
+++ config.fish
@@ -9,14 +9,15 @@ set fish_greeting ""
 # oh-my-fish
 . $fish_path/oh-my-fish.fish
 
-# Environment
 set -x EDITOR mvim
 set -x BUNDLER_EDITOR mvim
 set -x LOLCOMMITS_FORK true
 set -x PATH "/usr/local/heroku/bin:$PATH"
 set -x Z_SCRIPT_PATH (brew --prefix)/etc/profile.d/z.sh
 
-# Aliases
 alias be 'bundle exec'
 alias ve 'vagrant exec'
 alias vim 'mvim'
+alias wow 'git status'
+alias such 'git'
+alias very 'git'
