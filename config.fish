2cff1f5cd298bfd9b4d3d85a419e10bff15f73f9
Alex Rodionov <p0deje@gmail.com>
2014-11-02 13:17:31 +0600

    [fish] Rake plugin is deprecated

--- config.fish
+++ config.fish
@@ -3,7 +3,7 @@ set fish_path $HOME/.oh-my-fish
 
 # Fish configuration
 set fish_theme agnoster
-set fish_plugins brew extract rake rbenv z
+set fish_plugins brew extract rbenv z
 set fish_greeting ""
 function fish_title
   basename {$PWD}
