a77731aaa72edaf5478fc1986eaacb05a2927ee4
Alex Rodionov <p0deje@gmail.com>
2016-05-02 15:16:43 +0600

    [fish] black and white colors for fzf

--- .config/omf/before.init.fish
+++ .config/omf/before.init.fish
@@ -5,10 +5,9 @@ end
 set -x ANSIBLE_NOCOWS 1
 set -x BUNDLER_EDITOR mvim
 set -x EDITOR mvim
-set -x FZF_DEFAULT_OPTS '-e --reverse'
+set -x FZF_DEFAULT_OPTS "--exact --reverse --color=bw"
 set -x JAVA_HOME (/usr/libexec/java_home)
 set -x LOLCOMMITS_FORK true
 set -x Z_SCRIPT_PATH (brew --prefix)/etc/profile.d/z.sh
 
 set -gx PATH $PATH /usr/local/share/npm/bin
-
