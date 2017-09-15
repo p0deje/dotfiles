a8ed4d70e34b72e5387747e620863ee01bf0c21c
Alex Rodionov <p0deje@gmail.com>
2017-09-15 15:37:52 +0600

    [hammerspoon] Add mapping to make fullscreen window and undo

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -26,14 +26,43 @@ bindToLaunchOrFocus("b", "Slack")
 
 -- Window moving
 
+undo = {}
+
+function undo:push()
+  local win = hs.window.focusedWindow()
+  if win and not undo[win:id()] then
+    self[win:id()] = win:frame()
+  end
+end
+
+function undo:pop()
+  local win = hs.window.focusedWindow()
+  if win and self[win:id()] then
+    win:setFrame(self[win:id()])
+    self[win:id()] = nil
+  end
+end
+
 hs.hotkey.bind({"alt", "shift"}, "m", function()
+  undo:push()
   hs.window.focusedWindow():centerOnScreen()
 end)
 
+hs.hotkey.bind({"alt", "shift"}, "f", function()
+  undo:push()
+  hs.window.focusedWindow():moveToUnit(hs.layout.maximized)
+end)
+
 hs.hotkey.bind({"alt", "shift"}, "h", function()
+  undo:push()
   hs.window.focusedWindow():moveToUnit(hs.layout.left50)
 end)
 
 hs.hotkey.bind({"alt", "shift"}, "l", function()
+  undo:push()
   hs.window.focusedWindow():moveToUnit(hs.layout.right50)
+end)
+
+hs.hotkey.bind({"alt", "shift"}, "z", function()
+  undo:pop()
 end)
