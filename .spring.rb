aadb2dafcc6b3f2b52225f50a9cb974662b4293c
Alex Rodionov <p0deje@gmail.com>
2018-05-07 12:01:17 +0600

    [rbenv] Drop spring-watcher-listen
    It’s harder to make it work outside of bundler rather than tolerate
    polling listener

--- .spring.rb
+++ .spring.rb
@@ -1,11 +1,1 @@
 Spring.quiet = true
-
-begin
-  rb_inotify = Gem::Specification.find_all_by_name('rb-inotify').find { |gem| gem.version <= Gem::Version.new('0.10') }
-  rb_fsevent = Gem::Specification.find_all_by_name('rb-fsevent').find { |gem| gem.version <= Gem::Version.new('0.10') }
-  $LOAD_PATH << rb_inotify.full_require_paths.first
-  $LOAD_PATH << rb_fsevent.full_require_paths.first
-  require 'spring/watcher/listen'
-rescue
-  puts 'Failed to load spring-watcher-listen.'
-end
