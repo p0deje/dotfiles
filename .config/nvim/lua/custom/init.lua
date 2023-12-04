f47ce3444ff8a3c42374bb38acea9724e002d73b
Alex Rodionov <p0deje@gmail.com>
2023-12-04 07:59:48 -0800

    [nvim] Add custom textobjects for surround

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -87,3 +87,23 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.keymap.set("n", "q", "<CMD>q<CR>", { desc = "Quit", buffer = true, silent = true })
   end,
 })
+
+-- Text objects for custom characters.
+-- https://thevaluable.dev/vim-create-text-objects
+local chars = { "_", " ", ".", ":", ",", ";", "|", "/", "\\", "*", "+", "%", "`", "?" }
+for _, char in ipairs(chars) do
+  for _, mode in ipairs({ "x", "o" }) do
+    vim.api.nvim_set_keymap(
+      mode,
+      "i" .. char,
+      string.format(":<C-u>silent! normal! f%sF%slvt%s<CR>", char, char, char),
+      { noremap = true, silent = true }
+    )
+    vim.api.nvim_set_keymap(
+      mode,
+      "a" .. char,
+      string.format(":<C-u>silent! normal! f%sF%svf%s<CR>", char, char, char),
+      { noremap = true, silent = true }
+    )
+  end
+end
